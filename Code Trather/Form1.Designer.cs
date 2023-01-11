42b76b682510e1b526a19e4313851c1b9011f530
Meagan Kropp <meagan.kropp@gmail.com>
2023-01-11 16:26:59 -0600

    add functionality to save and run

--- Code Trather/Form1.Designer.cs
+++ Code Trather/Form1.Designer.cs
@@ -42,25 +42,29 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Location = new System.Drawing.Point(10, 9);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.Size = new System.Drawing.Size(82, 22);
             this.button1.TabIndex = 0;
             this.button1.Text = "Run";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(127, 12);
+            this.button2.Location = new System.Drawing.Point(111, 9);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(94, 29);
+            this.button2.Size = new System.Drawing.Size(82, 22);
             this.button2.TabIndex = 1;
             this.button2.Text = "Unit Test";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // splitContainer1
             // 
-            this.splitContainer1.Location = new System.Drawing.Point(12, 56);
+            this.splitContainer1.Location = new System.Drawing.Point(10, 42);
+            this.splitContainer1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.splitContainer1.Name = "splitContainer1";
             // 
             // splitContainer1.Panel1
@@ -71,8 +75,8 @@
             // 
             this.splitContainer1.Panel2.Controls.Add(this.textOutput);
             this.splitContainer1.Panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.splitContainer1_Panel2_Paint);
-            this.splitContainer1.Size = new System.Drawing.Size(1335, 649);
-            this.splitContainer1.SplitterDistance = 915;
+            this.splitContainer1.Size = new System.Drawing.Size(1168, 487);
+            this.splitContainer1.SplitterDistance = 800;
             this.splitContainer1.TabIndex = 2;
             this.splitContainer1.SplitterMoved += new System.Windows.Forms.SplitterEventHandler(this.splitContainer1_SplitterMoved);
             // 
@@ -81,9 +85,10 @@
             this.textInput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.textInput.Location = new System.Drawing.Point(3, 3);
+            this.textInput.Location = new System.Drawing.Point(3, 2);
+            this.textInput.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textInput.Name = "textInput";
-            this.textInput.Size = new System.Drawing.Size(909, 643);
+            this.textInput.Size = new System.Drawing.Size(795, 483);
             this.textInput.TabIndex = 3;
             this.textInput.Text = "";
             // 
@@ -92,31 +97,35 @@
             this.textOutput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.textOutput.Location = new System.Drawing.Point(3, 3);
+            this.textOutput.Location = new System.Drawing.Point(3, 2);
+            this.textOutput.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textOutput.Multiline = true;
             this.textOutput.Name = "textOutput";
             this.textOutput.ReadOnly = true;
-            this.textOutput.Size = new System.Drawing.Size(410, 643);
+            this.textOutput.Size = new System.Drawing.Size(359, 483);
             this.textOutput.TabIndex = 0;
             // 
             // saveButton
             // 
-            this.saveButton.Location = new System.Drawing.Point(1253, 12);
+            this.saveButton.Location = new System.Drawing.Point(1096, 9);
+            this.saveButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.saveButton.Name = "saveButton";
-            this.saveButton.Size = new System.Drawing.Size(94, 29);
+            this.saveButton.Size = new System.Drawing.Size(82, 22);
             this.saveButton.TabIndex = 3;
             this.saveButton.Text = "Save";
             this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1359, 729);
+            this.ClientSize = new System.Drawing.Size(1189, 547);
             this.Controls.Add(this.saveButton);
             this.Controls.Add(this.splitContainer1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.splitContainer1.Panel1.ResumeLayout(false);
--- Code Trather/Form1.cs
+++ Code Trather/Form1.cs
@@ -5,6 +5,8 @@ namespace Code_Trather
         public Form1()
         {
             InitializeComponent();
+            string download = Environment.GetEnvironmentVariable("USERPROFILE") + @"\" + "Downloads/file.py";
+            File.Create(download).Close();
         }
 
         private void splitContainer1_SplitterMoved(object sender, SplitterEventArgs e)
@@ -14,6 +16,24 @@ namespace Code_Trather
 
         private void splitContainer1_Panel2_Paint(object sender, PaintEventArgs e)
         {
+
+        }
+
+        private void saveButton_Click(object sender, EventArgs e) {
+            File.WriteAllText(Environment.GetEnvironmentVariable("USERPROFILE") + @"\" + "Downloads/file.py", textInput.Text);
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            System.Diagnostics.Process pProcess = new System.Diagnostics.Process();
+            pProcess.StartInfo.FileName = "cmd.exe";
+            pProcess.StartInfo.Arguments = "/C python " + Environment.GetEnvironmentVariable("USERPROFILE") + @"\" + "Downloads/file.py";
+            pProcess.StartInfo.RedirectStandardOutput = true;
+            pProcess.StartInfo.RedirectStandardError = true;
+            pProcess.Start();
+            string output = pProcess.StandardOutput.ReadToEnd();
+            string error = pProcess.StandardError.ReadToEnd();
+            pProcess.WaitForExit();
+            textOutput.Text = output + error;
 
         }
     }
