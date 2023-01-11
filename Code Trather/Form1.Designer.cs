3ec0b8f2dab328dd843e6bf6794b874956452080
Meagan Kropp <meagan.kropp@gmail.com>
2023-01-11 16:03:24 -0600

    test features

--- Code Trather/Form1.Designer.cs
+++ Code Trather/Form1.Designer.cs
@@ -28,12 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Save = new System.Windows.Forms.Button();
+            this.Run = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(77, 139);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(287, 268);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Save
+            // 
+            this.Save.Location = new System.Drawing.Point(24, 30);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(186, 68);
+            this.Save.TabIndex = 1;
+            this.Save.Text = "button1";
+            this.Save.UseVisualStyleBackColor = true;
+            this.Save.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Run
+            // 
+            this.Run.Location = new System.Drawing.Point(232, 30);
+            this.Run.Name = "Run";
+            this.Run.Size = new System.Drawing.Size(194, 68);
+            this.Run.TabIndex = 3;
+            this.Run.Text = "button2";
+            this.Run.UseVisualStyleBackColor = true;
+            this.Run.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(416, 139);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(333, 265);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Run);
+            this.Controls.Add(this.Save);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private TextBox textBox1;
+        private Button Save;
+        private Button Run;
+        private Label label1;
     }
 }
--- Code Trather/Form1.cs
+++ Code Trather/Form1.cs
@@ -2,9 +2,35 @@ namespace Code_Trather
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
+            string download = Environment.GetEnvironmentVariable("USERPROFILE") + @"\" + "Downloads/file.py";
+            //File.Create("C:/Users/meaga/Desktop/file.py");
+            //string file = download + "/file.py";
+            File.Create(download).Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            File.WriteAllText(Environment.GetEnvironmentVariable("USERPROFILE") + @"\" + "Downloads/file.py", textBox1.Text);
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e) {
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            System.Diagnostics.Process pProcess = new System.Diagnostics.Process();
+            pProcess.StartInfo.FileName = "cmd.exe";
+            pProcess.StartInfo.Arguments = "/C python " + Environment.GetEnvironmentVariable("USERPROFILE") + @"\" + "Downloads/file.py";
+            pProcess.StartInfo.RedirectStandardOutput = true;
+            pProcess.StartInfo.RedirectStandardError = true;
+            pProcess.Start();
+            string output = pProcess.StandardOutput.ReadToEnd();
+            string error = pProcess.StandardError.ReadToEnd();
+            pProcess.WaitForExit();
+            label1.Text = output + error;
         }
     }
 }
