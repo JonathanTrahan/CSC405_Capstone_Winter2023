b77d3bef7007d475ba799e5128eb4bde160672fd
Jonathan Trahan <jet055@latech.edu>
2023-02-03 16:45:38 -0600

    added python style colors

--- Code Trather/Form1.cs
+++ Code Trather/Form1.cs
@@ -47,31 +47,29 @@ namespace Code_Trather
             textInput.StyleResetDefault();
             textInput.Styles[Style.Default].Font = "Consolas";
             textInput.Styles[Style.Default].Size = 10;
-            textInput.Styles[Style.Default].BackColor = IntToColor(0x212121);
-            textInput.Styles[Style.Default].ForeColor = IntToColor(0xFFFFFF);
+            textInput.Styles[Style.Default].BackColor = IntToColor(0xFFFFFF);
+            textInput.Styles[Style.Default].ForeColor = IntToColor(0x000000);
             textInput.StyleClearAll();
 
-            // Configure the CPP (C#) lexer styles
-            textInput.Styles[Style.Cpp.Identifier].ForeColor = IntToColor(0xD0DAE2);
-            textInput.Styles[Style.Cpp.Comment].ForeColor = IntToColor(0xBD758B);
-            textInput.Styles[Style.Cpp.CommentLine].ForeColor = IntToColor(0x40BF57);
-            textInput.Styles[Style.Cpp.CommentDoc].ForeColor = IntToColor(0x2FAE35);
-            textInput.Styles[Style.Cpp.Number].ForeColor = IntToColor(0xFFFF00);
-            textInput.Styles[Style.Cpp.String].ForeColor = IntToColor(0xFFFF00);
-            textInput.Styles[Style.Cpp.Character].ForeColor = IntToColor(0xE95454);
-            textInput.Styles[Style.Cpp.Preprocessor].ForeColor = IntToColor(0x8AAFEE);
-            textInput.Styles[Style.Cpp.Operator].ForeColor = IntToColor(0xE0E0E0);
-            textInput.Styles[Style.Cpp.Regex].ForeColor = IntToColor(0xff00ff);
-            textInput.Styles[Style.Cpp.CommentLineDoc].ForeColor = IntToColor(0x77A7DB);
-            textInput.Styles[Style.Cpp.Word].ForeColor = IntToColor(0x48A8EE);
-            textInput.Styles[Style.Cpp.Word2].ForeColor = IntToColor(0xF98906);
-            textInput.Styles[Style.Cpp.CommentDocKeyword].ForeColor = IntToColor(0xB3D991);
-            textInput.Styles[Style.Cpp.CommentDocKeywordError].ForeColor = IntToColor(0xFF0000);
-            textInput.Styles[Style.Cpp.GlobalClass].ForeColor = IntToColor(0x48A8EE);
+            // Configure the Python lexer styles
+            textInput.Styles[Style.Python.Character].ForeColor = IntToColor(0x00AA00);
+            textInput.Styles[Style.Python.ClassName].ForeColor = IntToColor(0x0000FF);
+            textInput.Styles[Style.Python.ClassName].Bold = true;
+            textInput.Styles[Style.Python.CommentBlock].ForeColor = IntToColor(0xDD0000);
+            textInput.Styles[Style.Python.CommentLine].ForeColor = IntToColor(0xDD0000);
+            textInput.Styles[Style.Python.Decorator].ForeColor = IntToColor(0xFF8000);
+            textInput.Styles[Style.Python.DefName].ForeColor = IntToColor(0x0000FF);
+            textInput.Styles[Style.Python.Identifier].ForeColor = IntToColor(0x000000);
+            textInput.Styles[Style.Python.Number].ForeColor = IntToColor(0x000000);
+            textInput.Styles[Style.Python.Operator].ForeColor = IntToColor(0x000000);
+            textInput.Styles[Style.Python.String].ForeColor = IntToColor(0x00AA00);
+            textInput.Styles[Style.Python.StringEol].ForeColor = IntToColor(0x00AA00);
+            textInput.Styles[Style.Python.Triple].ForeColor = IntToColor(0x00AA00);
+            textInput.Styles[Style.Python.TripleDouble].ForeColor = IntToColor(0x00AA00);
+            textInput.Styles[Style.Python.Word].ForeColor = IntToColor(0xFF7700);
+            textInput.Styles[Style.Python.Word2].ForeColor = IntToColor(0x900090);
 
-            textInput.Styles[Style.Python.String].ForeColor = Color.Green;
-
-            textInput.Lexer = Lexer.Cpp;
+            textInput.Lexer = Lexer.Python;
 
             textInput.SetKeywords(0, "class extends implements import interface new case do while else if for in switch throw get set function var try catch finally while with default break continue delete return each const namespace package include use is as instanceof typeof author copy default deprecated eventType example exampleText exception haxe inheritDoc internal link mtasc mxmlc param private return see serial serialData serialField since throws usage version langversion playerversion productversion dynamic private public partial static intrinsic internal native override protected AS3 final super this arguments null Infinity NaN undefined true false abstract as base bool break by byte case catch char checked class const continue decimal default delegate do double descending explicit event extern else enum false finally fixed float for foreach from goto group if implicit in int interface internal into is lock long new null namespace object operator out override orderby params private protected public readonly ref return switch struct sbyte sealed short sizeof stackalloc static string select this throw true try typeof uint ulong unchecked unsafe ushort using var virtual volatile void while where yield");
             textInput.SetKeywords(1, "void Null ArgumentError arguments Array Boolean Class Date DefinitionError Error EvalError Function int Math Namespace Number Object RangeError ReferenceError RegExp SecurityError String SyntaxError TypeError uint XML XMLList Boolean Byte Char DateTime Decimal Double Int16 Int32 Int64 IntPtr SByte Single UInt16 UInt32 UInt64 UIntPtr Void Path File System Windows Forms ScintillaNET");
