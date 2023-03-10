7fe3beb9b886fb3f28dc028bc831f66179d1bdfe
Jonathan Trahan <jet055@latech.edu>
2023-03-10 16:50:45 -0600

    made changes to AES encrypt

--- Code Trather/Cryptog.cs
+++ Code Trather/Cryptog.cs
@@ -5,40 +5,53 @@ using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Code_Trather
 {
     internal class Cryptog
     {
         ///<summary>
-        /// Steve Lydford - 12/05/2008.
-        ///
-        /// Encrypts a file using Rijndael algorithm.
+        /// Encrypts a file using C# AES class.
+        /// inputFile is the path to the file to be encrypted.
+        /// outputFile is the path to where the encrypted file is written to.
+        /// Key and IV are the AES parameters.
         ///</summary>
         ///<param name="inputFile"></param>
         ///<param name="outputFile"></param>
-        public static void EncryptFile(string inputFile, string outputFile)
+        ///<param name="Key"></param>
+        ///<param name="IV"></param>
+        public static void EncryptFile(string inputFile, string outputFile, byte[] Key, byte[] IV)
         {
+            // Check arguments.
+            if (inputFile == null || inputFile.Length <= 0)
+                throw new ArgumentNullException("inputFile");
+            if (outputFile == null || outputFile.Length <= 0)
+                throw new ArgumentNullException("outputFile");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentNullException("IV");
+            byte[] encrypted;
 
-            try
+            // Create an Aes object
+            // with the specified key and IV.
+            using (Aes aesAlg = Aes.Create())
             {
-                string password = @"myKey123"; // Your Key Here
-                UnicodeEncoding UE = new UnicodeEncoding();
-                byte[] key = UE.GetBytes(password);
+                aesAlg.Key = Key;
+                aesAlg.IV = IV;
 
-                string cryptFile = outputFile;
-                FileStream fsCrypt = new FileStream(cryptFile, FileMode.Create);
+                // Create an encryptor to perform the stream transform.
+                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
 
-                #pragma warning disable SYSLIB0022 // Type or member is obsolete
-                RijndaelManaged RMCrypto = new RijndaelManaged();
-                #pragma warning restore SYSLIB0022 // Type or member is obsolete
+                try 
+                {
+                FileStream fsCrypt = new FileStream(outputFile, FileMode.Create);
 
-                CryptoStream cs = new CryptoStream(fsCrypt,
-                    RMCrypto.CreateEncryptor(key, key),
-                    CryptoStreamMode.Write);
+                CryptoStream cs = new CryptoStream(fsCrypt, encryptor, CryptoStreamMode.Write);
 
                 FileStream fsIn = new FileStream(inputFile, FileMode.Open);
-
+                
                 int data;
                 while ((data = fsIn.ReadByte()) != -1)
                     cs.WriteByte((byte)data);
@@ -47,12 +60,14 @@ namespace Code_Trather
                 fsIn.Close();
                 cs.Close();
                 fsCrypt.Close();
-            }
-            catch
-            {
-                MessageBox.Show("Encryption failed!", "Error");
+                }
+                catch
+                {
+                    MessageBox.Show("Encryption failed!", "Error");
+                }
             }
         }
+
         ///<summary>
         /// Steve Lydford - 12/05/2008.
         ///
@@ -136,5 +151,84 @@ namespace Code_Trather
 
             //DecryptFile(new FileInfo(Globals.encryptedZip));
         }*/
+
+        public static void RSAEncryptFile(string inputFile, string outputFile)
+        {
+
+            try
+            {
+                string password = @"myKey123"; // Your Key Here
+                UnicodeEncoding UE = new UnicodeEncoding();
+                byte[] key = UE.GetBytes(password);
+
+                string cryptFile = outputFile;
+                FileStream fsCrypt = new FileStream(cryptFile, FileMode.Create);
+
+                #pragma warning disable SYSLIB0022 // Type or member is obsolete
+                RijndaelManaged RMCrypto = new RijndaelManaged();
+                #pragma warning restore SYSLIB0022 // Type or member is obsolete
+
+                CryptoStream cs = new CryptoStream(fsCrypt,
+                    RMCrypto.CreateEncryptor(key, key),
+                    CryptoStreamMode.Write);
+
+                FileStream fsIn = new FileStream(inputFile, FileMode.Open);
+
+                int data;
+                while ((data = fsIn.ReadByte()) != -1)
+                    cs.WriteByte((byte)data);
+
+
+                fsIn.Close();
+                cs.Close();
+                fsCrypt.Close();
+            }
+            catch
+            {
+                MessageBox.Show("Encryption failed!", "Error");
+            }
+        }
+        ///<summary>
+        /// Steve Lydford - 12/05/2008.
+        ///
+        /// Decrypts a file using Rijndael algorithm.
+        ///</summary>
+        ///<param name="inputFile"></param>
+        ///<param name="outputFile"></param>
+        public static void RSADecryptFile(string inputFile, string outputFile)
+        {
+            string pass = @"myKey123"; // Your Key Here
+            string password = @$"{Interaction.InputBox("Password", "Decrypt File", "")}";
+
+            if (pass == password)
+            {
+                UnicodeEncoding UE = new UnicodeEncoding();
+                byte[] key = UE.GetBytes(password);
+
+                FileStream fsCrypt = new FileStream(inputFile, FileMode.Open);
+
+                #pragma warning disable SYSLIB0022 // Type or member is obsolete
+                RijndaelManaged RMCrypto = new RijndaelManaged();
+                #pragma warning restore SYSLIB0022 // Type or member is obsolete
+
+                CryptoStream cs = new CryptoStream(fsCrypt,
+                    RMCrypto.CreateDecryptor(key, key),
+                    CryptoStreamMode.Read);
+
+                FileStream fsOut = new FileStream(outputFile, FileMode.Create);
+
+                int data;
+                while ((data = cs.ReadByte()) != -1)
+                    fsOut.WriteByte((byte)data);
+
+                fsOut.Close();
+                cs.Close();
+                fsCrypt.Close();
+            }
+            else
+            {
+                MessageBox.Show("Wrong Password");
+            }
+        }
     }
 }
